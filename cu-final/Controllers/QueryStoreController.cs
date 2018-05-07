b423f1c96fe42b7bea75535032aaf00646d5812f
Gautam Krishnan Chittaranjana <e0146858@u.nus.edu>
2018-05-07 11:03:20 +0800

    Query List page done

--- cu-final/Controllers/QueryStoreController.cs
+++ cu-final/Controllers/QueryStoreController.cs
@@ -32,7 +32,15 @@ namespace ContosoUniversity.Controllers
                 await conn.OpenAsync();
                 using (var command = conn.CreateCommand())
                 {
-                    string query = "SELECT * FROM sys.query_store_query_text";
+                    string query = "SELECT TOP 25 rs.avg_physical_io_reads, rs.avg_duration, qt.query_sql_text, q.query_id, qt.query_text_id, p.plan_id, rs.runtime_stats_id, rs.avg_rowcount, rs.count_executions " +
+                                   "FROM sys.query_store_query_text AS qt "+
+                                   "JOIN sys.query_store_query AS q "+
+                                   "ON qt.query_text_id = q.query_text_id "+
+                                   "JOIN sys.query_store_plan AS p "+
+                                   "ON q.query_id = p.query_id "+
+                                   "JOIN sys.query_store_runtime_stats AS rs "+
+                                   "ON p.plan_id = rs.plan_id "+
+                                   "ORDER BY rs.avg_duration DESC";
                     command.CommandText = query;
                     DbDataReader reader = await command.ExecuteReaderAsync();
 
@@ -40,7 +48,15 @@ namespace ContosoUniversity.Controllers
                     {
                         while (await reader.ReadAsync())
                         {
-                            var row = new QueryStore { query_text_id = reader.GetInt64(0) };
+                            var row = new QueryStore { avg_physical_io_reads = reader.GetDouble(0),
+                                                       avg_duration = reader.GetDouble(1),
+                                                       query_sql_text = reader.GetString(2),
+                                                       query_id = reader.GetInt64(3),
+                                                       query_text_id = reader.GetInt64(4),
+                                                       plan_id = reader.GetInt64(5),
+                                                       runtime_stats_id = reader.GetInt64(6),
+                                                       avg_rowcount = reader.GetDouble(7),
+                                                       count_executions = reader.GetInt64(8)};
                             groups.Add(row);
                         }
                     }
--- cu-final/Models/QueryStoreModels/QueryStore.cs
+++ cu-final/Models/QueryStoreModels/QueryStore.cs
@@ -7,10 +7,22 @@ namespace ContosoUniversity.Models.QueryStoreModels
 {
     public class QueryStore
     {
-        public Int64 query_text_id { get; set; }
+        public Double avg_physical_io_reads { get; set; }
+
+        public Double avg_duration { get; set; }
 
         public String query_sql_text { get; set; }
 
-        public String statement_sql_handle { get; set; }
+        public Int64 query_id { get; set; }
+
+        public Int64 query_text_id { get; set; }
+
+        public Int64 plan_id { get; set; }
+       
+        public Int64 runtime_stats_id { get; set; }
+
+        public Double avg_rowcount { get; set; }
+
+        public Int64 count_executions { get; set; }
     }
 }
